e4f4a98163c05b7a5e6b9375c2b9a957e04cc951
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-02 21:47:40 +0200

    typed js cdn

---
+++ script.js
@@ -0,0 +1,7 @@
+let typed = new Typed(".multiple-text", {
+  strings: ["Frontend Developer", "Graphic Designer", "Music Producer"],
+  typedSpeed: 100,
+  backSpeed: 50,
+  backDelay: 1000,
+  loop: true,
+});
